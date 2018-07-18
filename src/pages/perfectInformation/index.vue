165df3a5bfea4f802d3131bc358383dd72cfb492
靓仔 <1147640042@qq.com>
2018-07-18 14:14:48 +0800

    commit

--- src/pages/perfectInformation/index.vue
+++ src/pages/perfectInformation/index.vue
@@ -1,18 +1,20 @@
 <template>
   <div class="perfectInfo-wrap">
+    <h3>Hi 请完善信息</h3>
+    <h5>完善信息，领取红包哟！</h5>
     <!-- 头像 -->
     <div class="head-pic">
-      <img src="" alt="">
-      <div class="tip">
-        <div class="icon-pic"><img src="" alt=""></div>
-        <span>添加头像</span>
+      <img :src="headPic" alt="" v-show="headPic">
+      <div class="tip" v-show="!headPic">
+        <img src="../../../static/images/icon-camera.png" alt="">
+        <div>添加头像</div>
       </div>
     </div>
     <!-- 表单 -->
     <div class="form">
       <div class="name">
         <span>姓名</span>
-        <input type="text" v-html="name">
+        <input type="text" v-model="name">
       </div>
       <div class="birthday">
         <span>出生日期</span>
@@ -21,8 +23,16 @@
         </picker>
       </div>
       <div class="sex">
-        <div class="sex-item boy">男</div>
-        <div class="sex-item girl">女</div>
+        <div class="sex-item"
+             v-for="(item,index) in sexArr"
+             v-text="item"
+             :class="[ sexStatus == index ? 'current' : '' ]"
+             @click="sexChange(index)"
+             :key="index"></div>
+      </div>
+      <!-- 确认按钮 -->
+      <div class="btn-sure">
+        #_#
       </div>
     </div>
   </div>
@@ -35,7 +45,9 @@
       return {
         imgUrl: [],
         name: '张三',
-        birthday: '2000-00-00'
+        birthday: '2000-00-00',
+        sexStatus: -1,
+        sexArr: ['男','女']
       }
     },
     methods: {
@@ -45,16 +57,98 @@
       birthdayChange(e){
         this.birthday = e.mp.detail.value
       },
+      sexChange(index){
+        this.sexStatus = index
+      }
     }
   }
 </script>
 
 <style lang="stylus" scoped>
   .perfectInfo-wrap {
+    h3 {
+      font-size: 40rpx;
+      color: #000;
+      text-align: center;
+    }
+    h5 {
+      font-size: 20rpx;
+      color: #000;
+      text-align: center;
+    }
     // 头像
     .head-pic {
-      img {
-
+      width: 180rpx;
+      height: 180rpx;
+      border: 10rpx solid #efeff4;
+      border-radius: 50%;
+      margin: 70rpx auto 145rpx;
+      .tip {
+        width: 100%;
+        height: 100%;
+        text-align: center;
+        img {
+          width: 30rpx;
+          height: 25rpx;
+          margin: 60rpx 0 10rpx;
+        }
+        div {
+          font-size: 20rpx;
+          color: #000;
+        }
+      }
+    }
+    // 表单
+    .form {
+      width: 500rpx;
+      margin: 0 auto;
+      .name, .birthday {
+        border: 2rpx solid #efeff4;
+        padding: 0 40rpx;
+        height: 100rpx;
+        line-height: 100rpx;
+        border-radius: 50rpx;
+        font-size: 30rpx;
+        color: #000;
+        display: flex;
+        justify-content space-between;
+        margin-bottom: 60rpx;
+        input {
+          height: 100%;
+          text-align: right;
+        }
+      }
+      .sex {
+        display: flex;
+        justify-content space-between
+        .sex-item {
+          width: 200rpx;
+          height: 100rpx;
+          border-radius: 50rpx;
+          border: 2rpx solid #efeff4;
+          text-align: center;
+          line-height: 100rpx;
+          font-size: 30rpx;
+          color: #000;
+        }
+        .current {
+          border-color: #ff4b2b;
+          background-color: #ffc9bf;
+          color: #ff4b2b;
+        }
+      }
+      // 确认按钮
+      .btn-sure {
+        width: 100rpx;
+        height: 100rpx;
+        border-radius: 50%;
+        background-color: #ff4b2b;
+        color: #fff;
+        font-size: 30rpx;
+        text-align: center;
+        line-height: 100rpx;
+        box-shadow: 0 0 20rpx rgba(255, 75, 43, 0.47);
+        margin: 100rpx auto 0;
       }
     }
   }
---
+++ src/pages/telRegister/index.vue
@@ -0,0 +1,117 @@
+<template>
+  <div class="telRegister-wrap">
+    <div class="form">
+      <!-- 输入手机号 -->
+      <div class="tel-number">
+        <div class="icon"></div>
+        <input type="text" placeholder="请输入手机号">
+      </div>
+      <!-- 获取验证码 -->
+      <div class="code">
+        <div class="icon"></div>
+        <input type="text" placeholder="请输入验证码">
+        <div class="get" @click="getCode" v-text="labelText"></div>
+      </div>
+    </div>
+    <div class="btn-next" @click="nextStep">下一步</div>
+  </div>
+</template>
+
+<script>
+  export default {
+    data(){
+      return {
+        labelText: '获取验证码'
+      }
+    },
+    methods: {
+      getCode(){
+//        msg('验证码已发送，请注意查收');
+        this.countDown = 60
+        this.timer = setInterval(()=>{
+          this.labelText = `${this.countDown} s`
+          this.countDown--
+          if(this.countDown < 0 ){
+            clearInterval(this.timer)
+            this.labelText = '重新获取'
+          }
+        },1000)
+      },
+      nextStep(){
+        console.log('下一步')
+      }
+    }
+  }
+</script>
+
+<style lang="stylus" scoped>
+  .telRegister-wrap {
+    .form {
+      // 输入手机号
+      .tel-number {
+        height: 100rpx;
+        border-top: 2rpx solid #efeff4;
+        border-bottom: 2rpx solid #efeff4;
+        padding: 0 40rpx;
+        .icon {
+          width: 33rpx;
+          height: 48rpx;
+          background-color: red;
+          margin-right: 40rpx;
+          float: left;
+          margin-top: 28rpx;
+        }
+        input {
+          height: 100%;
+          font-size: 30rpx;
+          color: #999;
+          float: left;
+        }
+      }
+      // 获取验证码
+      .code {
+        height: 100rpx;
+        border-bottom: 2rpx solid #efeff4;
+        padding: 0 40rpx;
+        .icon {
+          width: 39rpx;
+          height: 41rpx;
+          background-color: green;
+          float: left;
+          margin-top: 28rpx;
+          margin-right: 34rpx;
+        }
+        input {
+          height: 100%;
+          font-size: 30rpx;
+          color: #999;
+          float: left;
+        }
+        .get {
+          float: right;
+          width: 205rpx;
+          height: 63rpx;
+          line-height: 63rpx;
+          background-color: #ff4b2b;
+          color: #fff;
+          font-size: 30rpx;
+          text-align: center;
+          border-radius: 10rpx;
+          margin-top: 20rpx;
+        }
+      }
+    }
+    .btn-next {
+      width: 630rpx;
+      height: 80rpx;
+      line-height: 80rpx;
+      margin: 200rpx auto 0;
+      background-color: #ff4b2b;
+      text-align: center;
+      color: #fff;
+      font-size: 30rpx;
+      border-radius: 10rpx;
+    }
+  }
+</style>
+
